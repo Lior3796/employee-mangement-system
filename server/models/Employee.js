6e153498b1ad7a2564e80c5d9add55211dee992b
Lior3796 <liorlsa9@gmail.com>
2022-03-30 08:07:39 +0300

    re-create git folder locally

---
+++ server/models/Employee.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+const slugify = require("slugify");
+
+const EmployeeSchema = new mongoose.Schema({
+    _id: {
+        type: Number,
+        required: [true, "Please add a ID"],
+        unique: true,
+        length: [9, "Please add a 9 characters ID"]
+
+    },
+    Name: {
+        type: String,
+        required: [true, "Please add a name"],
+        unique: true,
+        trim: true,
+        maxlength: [50, "Name can not be more than 50 characters"],
+    },
+    slug: String,
+    email: {
+        type: String,
+        match: [
+            /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+            "Please add a valid email",
+        ],
+    },
+    address: {
+        type: String,
+        required: [true, "Please add an address"]
+    },
+
+
+})
+
+EmployeeSchema.pre("save", function (next) {
+    this.slug = slugify(this.name, { lower: true })
+    next();
+})
+
+
+module.exports = mongoose.model('Employee', EmployeeSchema);
